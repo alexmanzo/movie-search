b9385034c0f3b22505b465d8e63d6cd7b75027c6
alexmanzo <memanzo17@gmail.com>
2018-11-26 22:53:56 -0500

    added styling for search error

--- src/components/SearchError.js
+++ src/components/SearchError.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import  './SearchResults.css'
 
 function SearchError() {
     return (
+    <div className="results-err">
         <p>Something went wrong. Please enter a new search term.</p>
+    </div>
+        
     )
 }
 
